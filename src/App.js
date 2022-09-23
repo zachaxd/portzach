9eb7cbb0daebaa1e328438e7fccb609bb6e58e66
zachaxd <zachand9@gmail.com>
2022-09-23 00:28:36 -0500

    Add Navbar, mobile menu v1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home'
+import Projects from './Pages/Projects';
+import Experience from './Pages/Experience';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/projects' element={<Projects />} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Navbar() {
+    const [nav, setNav] = useState(false);
+    const handleClick = () => setNav(!nav);
+
+  return (
+    <div className='flex fixed justify-center items-center w-full h-[60px] px-4 text-sm backdrop-blur-xl border-b-2 border-gray-200'>
+        {/* toggle button */}
+        <div>
+            <button className='md:hidden absolute z-10'
+            onClick={handleClick}>
+                {!nav ? <MenuIcon /> : <CloseIcon />}
+            </button>
+        </div>
+        {/* links */}
+        <div className='hidden md:flex space-x-6'>
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/experience">Experience</Link>
+        </div>
+        {/* mobile menu */}
+       <div className={!nav ? 'hidden' : 'absolute top-0 right-0 shadow-md h-screen w-3/4 backdrop-blur-xl bg-gray-100 border-l-2 border-gray-200 text-gray-700 flex flex-col p-4 text-2xl items-center justify-center'}>
+       <Link to="/" className='w-full p-4 rounded border-b border-zinc-300 hover:bg-gray-200'>Home</Link>
+        <Link to="/projects" className='w-full p-4 rounded border-b border-zinc-300 hover:bg-gray-200'>Projects</Link>
+        <Link to="/experience" className='w-full p-4 rounded hover:bg-gray-200'>Experience</Link>
+       </div>
+    </div>
+  )
+}
+
+export default Navbar
